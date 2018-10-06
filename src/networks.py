b27d98093d65de3ad0b7ffa2ddc7f068ef8df07d
ScaMar <scamar@gmail.com>
2018-10-06 19:05:15 +0200

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -2,7 +2,7 @@
 
 # Dash src/chainparams.cpp
 params = {
-    'dash_main': {
+    'deviant_main': {
         'pubkey_address': 76, #L120
         'script_address': 16, #L122
         'genesis_hash': '00000e697a1e963f6ae8288419ffb0d59de9d232bac8efda8c6491d1c31e3139' #L110
