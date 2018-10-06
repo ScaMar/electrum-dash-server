90191d7ce625430b59bbfb3722c777ae43b6debd
ScaMar <scamar@gmail.com>
2018-10-06 19:32:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="electrum-dash-server",
+    name="electrum-deviant-server",
     version="1.0",
-    scripts=['run_electrum_dash_server','electrum-dash-server'],
+    scripts=['run_electrum_deviant_server','electrum-deviant-server'],
     install_requires=['plyvel','jsonrpclib', 'irc >= 11, <=14.0', 'x11_hash'],
     package_dir={
         'electrumserver':'src'
@@ -21,10 +21,10 @@ setup(
         'electrumserver.ircthread',
         'electrumserver.stratum_tcp'
     ],
-    description="Dash Electrum Server",
+    description="Deviant Electrum Server",
     author="Thomas Voegtlin ,ELM4ever, Propulsion, TheLazieR",
     author_email="thomasv@electrum.org, thelazier@gmail.com",
     license="MIT Licence",
-    url="https://github.com/spesmilo/electrum-server/ , https://github.com/thelazier/electrum-dash-server/",
-    long_description="""Server for the Electrum Lightweight Dash Wallet"""
+    url="https://github.com/spesmilo/electrum-server/ , https://github.com/thelazier/electrum-deviant-server/",
+    long_description="""Server for the Electrum Lightweight Deviant Wallet"""
 )
