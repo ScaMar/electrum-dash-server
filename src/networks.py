fa99ddd4f05731816f2c4ed5c8f1902beabfbc3a
ScaMar <scamar@gmail.com>
2018-10-06 19:03:17 +0200

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -5,11 +5,6 @@ params = {
     'dash_main': {
         'pubkey_address': 76, #L120
         'script_address': 16, #L122
-        'genesis_hash': '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6' #L110
+        'genesis_hash': '00000e697a1e963f6ae8288419ffb0d59de9d232bac8efda8c6491d1c31e3139' #L110
     },
-    'dash_test': {
-        'pubkey_address': 140, #L220
-        'script_address': 19, #L222
-        'genesis_hash': '00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c' #L210
-    }
 }
