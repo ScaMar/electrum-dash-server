a28acfb42dcdf4e203e07cafc01d8bc08089c7bb
ScaMar <scamar@gmail.com>
2018-10-06 19:49:39 +0200

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -1,10 +1,10 @@
 # Main network and testnet3 definitions
 
-# Dash src/chainparams.cpp
+# Deviant src/chainparams.cpp
 params = {
     'deviant_main': {
-        'pubkey_address': 76, #L120
-        'script_address': 16, #L122
-        'genesis_hash': '00000e697a1e963f6ae8288419ffb0d59de9d232bac8efda8c6491d1c31e3139' #L110
+        'pubkey_address': 90,
+        'script_address': 10,
+        'genesis_hash': '00000e697a1e963f6ae8288419ffb0d59de9d232bac8efda8c6491d1c31e3139'
     },
 }
